fbf86d71c3361a6d8a989d520897aeaccea9647d
New Author Name <email@address.example>
2024-07-07 19:56:29 +0530

    Setup Complete

---
+++ src/main/java/com/scm/Controller/PageController.java
@@ -0,0 +1,37 @@
+package com.scm.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+public class PageController {
+
+    
+    @RequestMapping("/home")
+    public String home(Model model){
+        System.out.println("Home page Handler");
+        model.addAttribute("name", "Anmol");
+        return "home";
+    }
+
+    // About route
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    // Service
+
+    @RequestMapping("/services")
+    public String servicesPage(){
+        System.out.println("Services page loading");
+        return "services";
+    }
+}
